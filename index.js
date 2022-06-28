3182f66116601c1875c4883f2a927aa46b26507d
Marc Sangster <computercampluv@gmail.com>
2022-06-28 12:02:19 +0100

    hello world express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+// const port = 3000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
