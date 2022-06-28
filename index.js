add5c434108c4dc21115a8dc2e128d23f96a0d10
Marc Sangster <computercampluv@gmail.com>
2022-06-28 12:09:15 +0100

    whoops

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-// const port = 3000;
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
