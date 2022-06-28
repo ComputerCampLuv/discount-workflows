aa7604dd9389408230291c5ad073ad39562e5368
Marc Sangster <computercampluv@gmail.com>
2022-06-28 13:39:48 +0100

    don't prompt when exipry present

--- index.js
+++ index.js
@@ -18,22 +18,30 @@ app.post('/', (req, res) => {
     req.body?.line_items[0]?.id
   ) {
     console.log(req.body?.line_items[0]);
-    res.json({
-      actions: [
-        {
-          type: "require_custom_fields",
-          title: "We need some information about this product",
-          message: "Bla bla bla",
-          entity: "line_item",
-          entity_id: req.body?.line_items[0].id,
-          required_custom_fields: [
-            {
-              name: "expiry_date"
-            }
-          ]
-        }
-      ]
-    });
+
+    const expiry = req.body?.line_items[0].custom_fields.find(({ name }) => name === 'expiry_date');
+
+    if (expiry.string_value && expiry.string_value.length > 0) {
+      res.json({ actions: [] });
+    } else {
+      res.json({
+        actions: [
+          {
+            type: "require_custom_fields",
+            title: "We need some information about this product",
+            message: "Bla bla bla",
+            entity: "line_item",
+            entity_id: req.body?.line_items[0].id,
+            required_custom_fields: [
+              {
+                name: "expiry_date"
+              }
+            ]
+          }
+        ]
+      });
+    }
+    
   } else {
     res.json({ actions: [] });
   }
