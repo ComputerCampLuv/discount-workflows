5a94cd28b024af295880f29d331ee749574dd186
Marc Sangster <computercampluv@gmail.com>
2022-06-28 16:22:48 +0100

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Decimal = require('decimal.js');
+const Decimal = require("decimal.js");
 
 let port = process.env.PORT;
 if (port == null || port == "") {
@@ -9,73 +9,154 @@ if (port == null || port == "") {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({ ok: true });
 });
 
-app.post('/', (req, res) => {
-  const lineItem = req.body.line_items[0];
 
-  console.log('line_item:', JSON.stringify(lineItem));
+app.post("/", (req, res) => {
+  const startOfDay = new Date();
+  startOfDay.setUTCHours(0, 0, 0, 0);
 
-  const expiryDate = lineItem
-    .custom_fields
-    .find(({ name }) => name === 'expiry_date');
+  const endOfDay = new Date();
+  endOfDay.setUTCHours(23, 59, 59, 999);
 
-  const expires = lineItem
-    .product
-    .custom_fields
-    .find(({ name }) => name === 'expires')
-    ?.boolean_value;
+  const actions = [];
 
-  if (!expires || lineItem.note === "Reduced") {
-    res.json({ actions: [] });
-  } else if (expiryDate) {
-    const today = new Date();
-    const expiry = new Date(expiryDate.string_value);
-    
-    if (
-      expiry.getYear() === today.getYear() &&
-      expiry.getMonth() === today.getMonth() &&
-      expiry.getDay() === today.getDay()
-    ) {
-      res.json({
-        actions: [
-          {
-            type: "remove_line_item",
-            line_item_id: lineItem.id
-          },
-          {
-            type: "add_line_item",
-            product_id: lineItem.product_id,
-            quantity: "1",
-            unit_price: new Decimal(lineItem.price_total).mul(0.75).toFixed(2),
-            note: "Reduced"
-          }
-        ]
-      });
-    } else {
-      res.json({ actions: [] });
+  req.body.line_items.forEach(lineItem => {
+    // Line item was created as a result of a previous action
+    if (lineItem.note === "Reduced") {
+      return;
     }
-  } else {
-    res.json({
-      actions: [
+
+    const productExpires = lineItem
+      .product
+      .custom_fields
+      .find(({ name }) => name === "expires")
+      ?.boolean_value || false;
+
+    // Product is one that does not expire
+    if (!productExpires) {
+      return;
+    }
+
+    const expiryDateField = lineItem
+      .custom_fields
+      .find(({ name }) => name === "expiry_date");
+
+    if (expiryDateField) {
+      const expiryDate = new Date(expiryDateField.string_value);
+
+      if (
+        expiryDate.getTime() > startOfDay &&
+        expiryDate.getTime() < endOfDay
+      ) {
+        // remove the line item with product nearing expiry
+        actions.push({
+          type: "remove_line_item",
+          line_item_id: lineItem.id
+        });
+        // replace the line item with a reduced variant
+        actions.push({
+          type: "add_line_item",
+          product_id: lineItem.product_id,
+          quantity: lineItem.price.quantity,
+          unit_price: new Decimal(lineItem.price).mul(0.75).toFixed(2),
+          note: "Reduced"
+        });
+      }
+      return;
+    }
+
+    // product expires but the exiry date is not yet known
+    actions.push({
+      type: "require_custom_fields",
+      title: "We need some information about this product",
+      message: "Please check the product expiry",
+      entity: "line_item",
+      entity_id: lineItem,
+      required_custom_fields: [
         {
-          type: "require_custom_fields",
-          title: "We need some information about this product",
-          message: "Bla bla bla",
-          entity: "line_item",
-          entity_id: req.body?.line_items[0].id,
-          required_custom_fields: [
-            {
-              name: "expiry_date"
-            }
-          ]
+          name: "expiry_date"
         }
       ]
     });
-  }
+  });
 });
+
+// app.post("/", (req, res) => {
+//   const startOfDay = new Date();
+//   startOfDay.setUTCHours(0, 0, 0, 0);
+//   // console.log(startOfDay);
+
+//   const endOfDay = new Date();
+//   endOfDay.setUTCHours(23, 59, 59, 999);
+//   // console.log(endOfDay);
+
+//   const lineItem = req.body.line_items[0];
+
+//   console.log("line_item:", JSON.stringify(lineItem));
+
+//   const expiryDate = lineItem
+//     .custom_fields
+//     .find(({ name }) => name === "expiry_date");
+
+//   const expires = lineItem
+//     .product
+//     .custom_fields
+//     .find(({ name }) => name === "expires")
+//     ?.boolean_value;
+
+//   if (
+//     !expires || lineItem.note === "Reduced"
+//   ) {
+//     res.json({ actions: [] });
+//   } else if (expiryDate) {
+//     const today = new Date();
+//     const expiry = new Date(expiryDate.string_value);
+    
+//     if (
+//       expiry.getYear() === today.getYear() &&
+//       expiry.getMonth() === today.getMonth() &&
+//       expiry.getDay() === today.getDay()
+//     ) {
+//       res.json({
+//         actions: [
+//           {
+//             type: "remove_line_item",
+//             line_item_id: lineItem.id
+//           },
+//           {
+//             type: "add_line_item",
+//             product_id: lineItem.product_id,
+//             quantity: "1",
+//             unit_price: new Decimal(lineItem.price_total).mul(0.75).toFixed(2),
+//             note: "Reduced"
+//           }
+//         ]
+//       });
+//     } else {
+//       res.json({ actions: [] });
+//     }
+//   } else {
+//     res.json({
+//       actions: [
+//         {
+//           type: "require_custom_fields",
+//           title: "We need some information about this product",
+//           message: "Bla bla bla",
+//           entity: "line_item",
+//           entity_id: req.body?.line_items[0].id,
+//           required_custom_fields: [
+//             {
+//               name: "expiry_date"
+//             }
+//           ]
+//         }
+//       ]
+//     });
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
