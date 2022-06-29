dab474e6f4b77f31bba298751a1dccff0adf2458
Marc Sangster <computercampluv@gmail.com>
2022-06-29 10:41:01 +0100

    get/post discounts

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const app = express();
 const Decimal = require("decimal.js");
+const axios = require('axios');
+const _ = require('lodash');
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+  port = 5000;
 }
 
-const discountRules = [
-  { days: 1, hours: 0, minutes: 0, reduction: 1 },     // "2022-06-30 00:00:00"
-  { days: 0, hours: 17, minutes: 0, reduction: 0.95 }, // "2022-06-29 17:00:00"
-  { days: 0, hours: 12, minutes: 0, reduction: 0.75 }, // "2022-06-29 12:00:00"
-  { days: 0, hours: 0, minutes: 0, reduction: 0.50 },  // "2022-06-29 00:00:00"
-  { days: -1, hours: 0, minutes: 0, reduction: 0.25 }, // "2022-06-28 17:00:00"
-  { days: -2, hours: 0, minutes: 0, reduction: 0.1 }   // "2022-06-27 17:00:00"
-];
+const discountRules = {
+  computercamplove: [
+    { days: 1, hours: 0, minutes: 0, reduction: 1 },
+    { days: 0, hours: 17, minutes: 0, reduction: 0.95 },
+    { days: 0, hours: 12, minutes: 0, reduction: 0.75 },
+    { days: 0, hours: 0, minutes: 0, reduction: 0.50 },
+    { days: -1, hours: 0, minutes: 0, reduction: 0.25 },
+    { days: -2, hours: 0, minutes: 0, reduction: 0.1 }
+  ]
+};
 
 const satisfiesRule = (expiry, rule) => {
   const now = new Date();
@@ -39,11 +43,101 @@ app.get("/", (req, res) => {
   res.json({ ok: true });
 });
 
+app.get("/discounts", (req, res) => {
+  res.json({ data: discountRules[req.query.domain_prefix] });
+});
+
+// {
+//   personal_token: '',
+//   domain_prefix: '',
+//   discounts: [
+//     {
+//       days: 0,
+//       hours: 0,
+//       minutes: 0,
+//       reduction: 0.5
+//     }
+//   ]
+// };
+app.post("/discounts", async (req, res) => {
+  const client = axios.create({
+    baseURL: `https://${req.body.domain_prefix}.vendhq.com/api/2.0`,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${req.body.personal_token}`
+    }
+  });
+
+  let customFields = (
+    await client.get("/workflows/custom_fields?entity=product")
+  ).data.data;
+
+  if (
+    !customFields.find(({ name }) => name === "expires")
+  ) {
+    // Add custom field to product if not already present
+    await client.post(
+      "/workflows/custom_fields",
+      {
+        entity: "product",
+        name: "expires",
+        title: "Expires",
+        type: "boolean",
+        visible_in_ui: true,
+        editable_in_ui: true,
+        print_on_receipt: false,
+      }
+    );
+  }
+
+  const remoteRules = (
+    await client.get("/workflows/remote_rules")
+  ).data.data;
+
+  let remoteRule = remoteRules.find(({ url }) => url === "https://nameless-sea-27513.herokuapp.com/");
+  if (!remoteRule) {
+    // Add remote rule if not already present
+    remoteRule = (
+      await client.post(
+        "/workflows/remote_rules",
+        { url: "https://nameless-sea-27513.herokuapp.com/" }
+      )
+    ).data.data;
+  }
+  
+  const rules = (
+    await client.get("/workflows/rules")
+  ).data.data;
+
+  if (
+    !rules.find((rule) => rule.event_type === "sale.line_items.added" && rule.remote_rule_id === remoteRule.id)
+  ) {
+    // Add rule if not already present
+    await client.post(
+      "/workflows/rules",
+      {
+        event_type: "sale.line_items.added",
+        remote_rule_id: remoteRule.id
+      }
+    );
+  }
+
+  const defaults = { days: 0, hours: 0, minutes: 0, reduction: 0 };
+  const discounts = _.map(
+    req.body.discounts,
+    (dc) => _.pick(
+      { ...defaults, ...dc },
+      ['days', 'hours', 'minutes', 'reduction']
+    )
+  );
+
+  discountRules[req.body.domain_prefix] = discounts;
+
+  res.json({ data: discounts });
+});
 
 app.post("/", (req, res) => {
-  const today = new Date().setUTCHours(0, 0, 0, 0);
-  const now = new Date();
-
   const actions = [];
 
   (req.body.line_items || []).forEach(lineItem => {
@@ -76,7 +170,7 @@ app.post("/", (req, res) => {
     if (expiryDateField) {
       const expiryDate = new Date(expiryDateField.string_value);
 
-      const discount = discountRules.find((rule) => satisfiesRule(expiryDate, rule));
+      const discount = discountRules[req.body.retailer.domain_prefix].find((rule) => satisfiesRule(expiryDate, rule));
 
       if (discount) {
         // remove the line item with product nearing expiry
